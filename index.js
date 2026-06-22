d05ed320a0c3390582c30f955bc65ac178ec294b
jehadabubakergame <jehadabubakergame@gmail.com>
2026-06-22 19:20:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1237,6 +1237,16 @@ if (imageTypes.includes(fileExt)) {
             embeds: [embed],
             files: [fileName]
         });
+        for (const msg of sortedMessages.values()) {
+    if (msg.attachments.size > 0) {
+        const files = msg.attachments.map(att => att.url);
+
+        await archiveChannel.send({
+            content: `📎 مرفقات من ${msg.author.tag}`,
+            files: files
+        }).catch(console.error);
+    }
+}
     }
 
     fs.unlinkSync(fileName);
